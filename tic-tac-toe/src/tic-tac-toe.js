f1695d94057c38526511627b71c751ab12d20ca8
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-17 22:38:14 +0100

    Update tic-tac-toe.js

--- tic-tac-toe/src/tic-tac-toe.js
+++ tic-tac-toe/src/tic-tac-toe.js
@@ -2,6 +2,69 @@ import React, { useState } from "react";
 
 export default function TicTacToe() {
   const [fields, setFields] = useState(Array(9).fill(null));
+  const [gameStatus, setGameStatus] = useState("");
+
+  function ChooseRandomField(fields) {
+    let isSet = false;
+    while (!isSet) {
+      const randomIndex = Math.floor(Math.random() * 9);
+      if (fields[randomIndex] === null) {
+        fields[randomIndex] = "O";
+        isSet = true;
+      }
+    }
+    return fields;
+  }
+
+  function Win(fields) {
+    for (let i = 0; i < 3; i++) {
+      if (
+        fields[i] === fields[i + 3] &&
+        fields[i] === fields[i + 6] &&
+        fields[i] !== null
+      ) {
+        return fields[i];
+      }
+      if (
+        fields[i * 3] === fields[i * 3 + 1] &&
+        fields[i * 3] === fields[i * 3 + 2] &&
+        fields[i * 3] !== null
+      ) {
+        return fields[i * 3];
+      }
+    }
+    if (
+      fields[0] === fields[4] &&
+      fields[0] === fields[8] &&
+      fields[0] !== null
+    ) {
+      return fields[0];
+    }
+    if (
+      fields[2] === fields[4] &&
+      fields[2] === fields[6] &&
+      fields[2] !== null
+    ) {
+      return fields[2];
+    }
+    return "";
+  }
+
+  function Field({ value, onClick }) {
+    return (
+      <button
+        style={{
+          width: "100px",
+          height: "100px",
+          fontSize: "50px",
+          margin: "10px",
+        }}
+        onClick={onClick}
+      >
+        {value}
+      </button>
+    );
+  }
 
   function handleClick(index) {
     let newFields = [...fields];
@@ -10,31 +73,21 @@ export default function TicTacToe() {
       newFields[index] = "X";
     }
 
-    setFields(newFields);
-
     const isWin = Win(newFields);
 
     if (isWin === "X") {
-      alert("You win");
+      setGameStatus("You win");
       setFields(Array(9).fill(null));
     } else {
       let nonNullFields = newFields.filter((field) => field !== null);
       if (nonNullFields.length === 9) {
-        alert("Draw");
+        setGameStatus("Draw");
         setFields(Array(9).fill(null));
       } else {
-        let isSet = false;
-        while (!isSet) {
-          const randomIndex = Math.floor(Math.random() * 9);
-          if (newFields[randomIndex] === null) {
-            newFields[randomIndex] = "O";
-            isSet = true;
-          }
-        }
-        setFields(newFields);
+        newFields = [...ChooseRandomField(newFields)];
         const isWin = Win(newFields);
         if (isWin === "O") {
-          alert("You lose");
+          setGameStatus("You lose");
           setFields(Array(9).fill(null));
         }
       }
@@ -54,56 +107,8 @@ export default function TicTacToe() {
       <Field value={fields[6]} onClick={() => handleClick(6)} />
       <Field value={fields[7]} onClick={() => handleClick(7)} />
       <Field value={fields[8]} onClick={() => handleClick(8)} />
+      <br />
+      <>{gameStatus}</>
     </>
-  );
-}
-
-function Win(fields) {
-  for (let i = 0; i < 3; i++) {
-    if (
-      fields[i] === fields[i + 3] &&
-      fields[i] === fields[i + 6] &&
-      fields[i] !== null
-    ) {
-      return fields[i];
-    }
-    if (
-      fields[i * 3] === fields[i * 3 + 1] &&
-      fields[i * 3] === fields[i * 3 + 2] &&
-      fields[i * 3] !== null
-    ) {
-      return fields[i * 3];
-    }
-  }
-  if (
-    fields[0] === fields[4] &&
-    fields[0] === fields[8] &&
-    fields[0] !== null
-  ) {
-    return fields[0];
-  }
-  if (
-    fields[2] === fields[4] &&
-    fields[2] === fields[6] &&
-    fields[2] !== null
-  ) {
-    return fields[2];
-  }
-  return "";
-}
-
-function Field({ value, onClick }) {
-  return (
-    <button
-      style={{
-        width: "100px",
-        height: "100px",
-        fontSize: "50px",
-        margin: "10px",
-      }}
-      onClick={onClick}
-    >
-      {value}
-    </button>
   );
 }
