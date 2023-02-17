68afefbf7911a64bd62f83f06be5870c0153bfbd
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-17 22:09:11 +0100

    Add logic

--- tic-tac-toe/src/tic-tac-toe.js
+++ tic-tac-toe/src/tic-tac-toe.js
@@ -1,39 +1,109 @@
 import React, { useState } from "react";
 
 export default function TicTacToe() {
+  const [fields, setFields] = useState(Array(9).fill(null));
+
+  function handleClick(index) {
+    let newFields = [...fields];
+
+    if (newFields[index] === null) {
+      newFields[index] = "X";
+    }
+
+    setFields(newFields);
+
+    const isWin = Win(newFields);
+
+    if (isWin === "X") {
+      alert("You win");
+      setFields(Array(9).fill(null));
+    } else {
+      let nonNullFields = newFields.filter((field) => field !== null);
+      if (nonNullFields.length === 9) {
+        alert("Draw");
+        setFields(Array(9).fill(null));
+      } else {
+        let isSet = false;
+        while (!isSet) {
+          const randomIndex = Math.floor(Math.random() * 9);
+          if (newFields[randomIndex] === null) {
+            newFields[randomIndex] = "O";
+            isSet = true;
+          }
+        }
+        setFields(newFields);
+        const isWin = Win(newFields);
+        if (isWin === "O") {
+          alert("You lose");
+          setFields(Array(9).fill(null));
+        }
+      }
+    }
+  }
+
   return (
     <>
-      <Field />
-      <Field />
-      <Field />
+      <Field value={fields[0]} onClick={() => handleClick(0)} />
+      <Field value={fields[1]} onClick={() => handleClick(1)} />
+      <Field value={fields[2]} onClick={() => handleClick(2)} />
       <br />
-      <Field />
-      <Field />
-      <Field />
+      <Field value={fields[3]} onClick={() => handleClick(3)} />
+      <Field value={fields[4]} onClick={() => handleClick(4)} />
+      <Field value={fields[5]} onClick={() => handleClick(5)} />
       <br />
-      <Field />
-      <Field />
-      <Field />
+      <Field value={fields[6]} onClick={() => handleClick(6)} />
+      <Field value={fields[7]} onClick={() => handleClick(7)} />
+      <Field value={fields[8]} onClick={() => handleClick(8)} />
     </>
   );
 }
 
-function Field() {
-  const [field, setField] = useState("");
+function Win(fields) {
+  for (let i = 0; i < 3; i++) {
+    if (
+      fields[i] === fields[i + 3] &&
+      fields[i] === fields[i + 6] &&
+      fields[i] !== null
+    ) {
+      return fields[i];
+    }
+    if (
+      fields[i * 3] === fields[i * 3 + 1] &&
+      fields[i * 3] === fields[i * 3 + 2] &&
+      fields[i * 3] !== null
+    ) {
+      return fields[i * 3];
+    }
+  }
+  if (
+    fields[0] === fields[4] &&
+    fields[0] === fields[8] &&
+    fields[0] !== null
+  ) {
+    return fields[0];
+  }
+  if (
+    fields[2] === fields[4] &&
+    fields[2] === fields[6] &&
+    fields[2] !== null
+  ) {
+    return fields[2];
+  }
+  return "";
+}
+
+function Field({ value, onClick }) {
   return (
     <button
-      className="field"
       style={{
         width: "100px",
         height: "100px",
         fontSize: "50px",
         margin: "10px",
       }}
-      onClick={() => {
-        setField(field === "" ? "X" : "");
-      }}
+      onClick={onClick}
     >
-      {field}
+      {value}
     </button>
   );
 }
