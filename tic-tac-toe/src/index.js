f9f97a06860b201cf31b3417a783bff5b24af0fd
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-19 12:55:52 +0100

    Update index.js

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import TicTacToe from "./tic-tac-toe";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
