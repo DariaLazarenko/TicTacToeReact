cf907d8e3d47c6df98f75492bac3a079316b263e
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-02-19 13:06:43 +0100

    Add comments

--- tic-tac-toe/src/tic-tac-toe.js
+++ tic-tac-toe/src/tic-tac-toe.js
@@ -5,8 +5,11 @@ export default function TicTacToe() {
   const [gameStatus, setGameStatus] = useState("");
 
   function FillRandomFieldWith0(fields) {
+    // create a variable to track if we have set a value
     let isSet = false;
+    // while we have not set a value
     while (!isSet) {
+      // get a random index between 0 and 8
       const randomIndex = Math.floor(Math.random() * 9);
       if (fields[randomIndex] === null) {
         fields[randomIndex] = "O";
@@ -18,6 +21,7 @@ export default function TicTacToe() {
 
   function Win(fields) {
     for (let i = 0; i < 3; i++) {
+      // check all rows
       if (
         fields[i] === fields[i + 3] &&
         fields[i] === fields[i + 6] &&
@@ -25,6 +29,7 @@ export default function TicTacToe() {
       ) {
         return fields[i];
       }
+      // check all columns
       if (
         fields[i * 3] === fields[i * 3 + 1] &&
         fields[i * 3] === fields[i * 3 + 2] &&
@@ -33,6 +38,7 @@ export default function TicTacToe() {
         return fields[i * 3];
       }
     }
+    // check both diagonals
     if (
       fields[0] === fields[4] &&
       fields[0] === fields[8] &&
@@ -47,9 +53,11 @@ export default function TicTacToe() {
     ) {
       return fields[2];
     }
+    // if we get here, no one has won
     return "";
   }
 
+  /* This is a function component that takes two props, value and onClick. The onClick function will be called when the button is clicked. */
   function Field({ value, onClick }) {
     return (
       <button
@@ -67,35 +75,56 @@ export default function TicTacToe() {
   }
 
   async function resetGame() {
+    // Wait a second
     await new Promise((resolve) => setTimeout(resolve, 1000));
+    // Clear the game status
     setGameStatus("");
+    // Clear the fields
     setFields(Array(9).fill(null));
   }
 
   function handleClick(index) {
+    // create a copy of fields array
     let newFields = [...fields];
 
+    // check if the clicked field is not already filled
     if (newFields[index] === null) {
+      // fill the clicked field with the "X"
       newFields[index] = "X";
     }
+    // update the fields array
     setFields(newFields);
 
+    // check if the game is finished
     const isWin = Win(newFields);
 
+    // if the game is finished
     if (isWin === "X") {
+      // set the game status to "You win"
       setGameStatus("You win");
+      // reset the game
       resetGame();
     } else {
+      // create a list of non-null fields
       let nonNullFields = newFields.filter((field) => field !== null);
+      // if the list of non-null fields has 9 elements
       if (nonNullFields.length === 9) {
+        // set the game status to "Draw"
         setGameStatus("Draw");
+        // reset the game
         resetGame();
       } else {
+        // fill a random empty field with the "0"
         newFields = [...FillRandomFieldWith0(newFields)];
+        // update the fields array
         setFields(newFields);
+        // check if the game is finished
         const isWin = Win(newFields);
+        // if the game is finished
         if (isWin === "O") {
+          // set the game status to "You lose"
           setGameStatus("You lose");
+          // reset the game
           resetGame();
         }
       }
